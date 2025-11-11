a0854bb824ee17353c66539a1d6f85a2d9036ec3
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-11 13:15:44 +0100

    Create GameData.cs
    Added GameData Class

---
+++ PopupMiniGames/GameData.cs
@@ -0,0 +1,13 @@
+﻿namespace MiniGames
+{
+    public class GameData
+    {
+        public int Score { get; set; } = 0;
+
+        public int Mistakes { get; set; } = 0;
+
+        public int MaxScore { get; set; } = 25;
+
+        public int MaxMistakes { get; set; } = 10;
+    }
+}
