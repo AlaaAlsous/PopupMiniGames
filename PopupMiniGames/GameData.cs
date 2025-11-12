cc55caa92713111b1bddaa89e54c100cda3d86a4
Alaa Alsous <lawyeralaa@gmail.com>
2025-11-12 13:38:03 +0100

    Add Difficulty property and SetDifficulty method to GameData

--- PopupMiniGames/GameData.cs
+++ PopupMiniGames/GameData.cs
@@ -6,8 +6,28 @@
 
         public int Mistakes { get; set; } = 0;
 
-        public int MaxScore { get; set; } = 25;
+        public int MaxScore { get; set; }
 
-        public int MaxMistakes { get; set; } = 10;
+        public int MaxMistakes { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public void SetDifficulty(Difficulty difficulty)
+        {
+            Difficulty = difficulty;
+            switch (difficulty)
+            {
+                case Difficulty.Easy:
+                    MaxScore = 15;
+                    MaxMistakes = 10;
+                    break;
+                case Difficulty.Medium:
+                    MaxScore = 25;
+                    MaxMistakes = 7;
+                    break;
+                case Difficulty.Hard:
+                    MaxScore = 35;
+                    MaxMistakes = 5;
+                    break;
+            }
+        }
     }
 }
