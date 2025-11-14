6ed51664212646995270af92e7e5b4a927046b3e
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-14 10:18:59 +0100

    Create MenuButton.cs
    Created MenuButton class

---
+++ PopupMiniGames/UI/MenuButton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopupMiniGames.UI
+{
+    internal class MenuButton : Button
+    {
+        private string selectedImagePath;
+        private string normalImagePath;
+    }
+}
