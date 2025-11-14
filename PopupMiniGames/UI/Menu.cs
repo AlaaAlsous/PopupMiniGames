ccfa94c6220e5ef9a6076487d743974b47abef67
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-14 10:24:19 +0100

    Created Menu Class

---
+++ PopupMiniGames/UI/Menu.cs
@@ -0,0 +1,20 @@
+﻿using MiniGames;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopupMiniGames.UI
+{
+    internal class Menu
+    {
+        List<MenuButton> menuButtons;
+        public int CurrentSelection { get; private set; }
+        public Menu(Form parentForm)
+        {
+            menuButtons = new List<MenuButton>();
+            CurrentSelection = -1;
+        }
+    }
+}
