41e11afae3fab3360716113628619fdd2eafe436
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-14 10:20:24 +0100

    Update MenuButton.cs
    Added Constructor To MenuButton class, mostly copied from UIManager

--- PopupMiniGames/UI/MenuButton.cs
+++ PopupMiniGames/UI/MenuButton.cs
@@ -10,5 +10,22 @@ namespace PopupMiniGames.UI
     {
         private string selectedImagePath;
         private string normalImagePath;
+        public MenuButton(string normalImage, string selectedImage, Point location, Action onClick, string basePath = "")
+        {
+            selectedImagePath = Path.Combine(basePath, selectedImage);
+            normalImagePath = Path.Combine(basePath, normalImage);
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            this.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            this.BackColor = Color.Transparent;
+            this.TabStop = false;
+            this.Width = 200;
+            this.Height = 80;
+            this.Location = location;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            this.Click += (s, e) => onClick();
+        }
     }
 }
