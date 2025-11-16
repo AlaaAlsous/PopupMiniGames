7731fcedcdd07d442dd2f9374d39998e700f0593
Alaa Alsous <lawyeralaa@gmail.com>
2025-11-16 16:46:59 +0100

    Initialize QuickMathGame class with basic fields and UI elements.

---
+++ PopupMiniGames/MiniGames/QuickMathGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using MiniGames;
+namespace MiniGames.MiniGames
+{
+    public class QuickMathGame : Form, IMiniGame, IMiniGameWithCleanup
+    {
+        public event EventHandler<GameResult>? GameEnded;
+        private Random rnd = new Random();
+        private int num1, num2, answer, timeLeft, score, wrongAnswer;
+        private Difficulty currentDifficulty;
+        private Label labelQuestion, labelTime, labelScore, labelWrong, currentDifficultyLabel;
+        private TextBox textBoxAnswer;
+        private Button buttonCheck;
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+    }
+}
