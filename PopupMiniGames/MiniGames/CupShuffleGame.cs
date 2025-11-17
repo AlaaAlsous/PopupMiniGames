93905ed6c10ca68777dc73e7afd5097c07100425
idaliaGX <idaliagock@gmail.com>
2025-11-17 16:07:16 +0100

    Add UI setup for cups, title image and instruction label

--- PopupMiniGames/MiniGames/CupShuffleGame.cs
+++ PopupMiniGames/MiniGames/CupShuffleGame.cs
@@ -21,7 +21,7 @@ namespace MiniGames.MiniGames
         private Difficulty currentDifficulty;
         private PictureBox titleImage = null!;
         private string assetsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\GameAssets");
-        
+
         public void StartGame(Difficulty difficulty)
         {
             currentDifficulty = difficulty;
@@ -29,6 +29,75 @@ namespace MiniGames.MiniGames
                 parentContainer = Application.OpenForms[0];
 
             SetupGameUI(difficulty);
+        }
+        private void SetupGameUI(Difficulty difficulty)
+        {
+            int cupCount = difficulty switch
+            {
+                Difficulty.Easy => 3,
+                Difficulty.Medium => 4,
+                Difficulty.Hard => 5,
+                _ => 3
+            };
+
+            if (instructionLabel == null)
+            {
+                instructionLabel = new Label
+                {
+                    Text = "Guess the correct cup!",
+                    AutoSize = false,
+                    Size = new Size(400, 80),
+                    Location = new Point((parentContainer.Width - 400) / 2, 10),
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    BackColor = Color.Transparent,
+                    Font = new Font("Arial", 16, FontStyle.Bold)
+                };
+
+                string titlePath = Path.Combine(assetsPath, "Title.png");
+                if (File.Exists(titlePath))
+                    titleImage.Image = Image.FromFile(titlePath);
+
+                parentContainer.Controls.Add(instructionLabel);
+                instructionLabel.BringToFront();
+            }
+
+            correctIndex = rand.Next(cupCount);
+
+            foreach (var c in cups)
+                parentContainer.Controls.Remove(c);
+            cups.Clear();
+
+            int startX = 50;
+            int spacing = 200;
+            int cupWidth = 180;
+            int cupHeight = 220;
+
+            correctIndex = rand.Next(cupCount);
+
+            for (int i = 0; i < cupCount; i++)
+            {
+                PictureBox cup = new PictureBox
+                {
+                    Width = cupWidth,
+                    Height = cupHeight,
+                    Location = new Point(startX + i * spacing, 200),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Cursor = Cursors.Hand,
+                    BorderStyle = BorderStyle.None,
+                    BackColor = Color.Transparent
+                };
+
+                string cupImage = Path.Combine(assetsPath, "BOX_CLOSE.png");
+                if (File.Exists(cupImage))
+                    cup.Image = Image.FromFile(cupImage);
+
+                int index = i;
+                cup.Click += (s, e) => OnCupSelected(index);
+
+                cups.Add(cup);
+                parentContainer.Controls.Add(cup);
+                cup.BringToFront();
+            }
         }
 
     }
