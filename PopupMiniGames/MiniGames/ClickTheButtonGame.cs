b36c59e4f9bc8da11a48e4f8c8524ea0783093a9
Alaa Alsous <lawyeralaa@gmail.com>
2025-11-18 10:06:48 +0100

    Create ClickTheButtonGame class with basic fields

---
+++ PopupMiniGames/MiniGames/ClickTheButtonGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using MiniGames;
+
+namespace MiniGames.MiniGames
+{
+    public class ClickTheButtonGame : Form, IMiniGame, IMiniGameWithCleanup
+    {
+        public event EventHandler<GameResult>? GameEnded;
+        private Button clickButton;
+        private Label labelTime, winClicksLebel, currentDifficultyLabel;
+        private Difficulty currentDifficulty;
+        private System.Windows.Forms.Timer timer;
+        private int timeLeft, clicks, winClicks = 0;
+        private Random place = new Random();
+    }
+}
