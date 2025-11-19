0784d7e1ae06d00326b182011e711424d9189ec0
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-19 14:56:48 +0100

    Added spawning logic to PipeSpawner

--- PopupMiniGames/GameAssets/FlappyKalleAssets/PipeSpawner.cs
+++ PopupMiniGames/GameAssets/FlappyKalleAssets/PipeSpawner.cs
@@ -11,12 +11,21 @@ namespace PopupMiniGames.GameAssets.FlappyKalleAssets
     internal class PipeSpawner : GameObject
     {
         public float TimeBetweenSpawns { get; set; } = 6;
+        public int MaxSpawnY { get; set; } = 100;
+        public int MinSpawnY { get; set; } = -300;
+        public int SpawnX { get; set; } = 1000;
+        public int MiddleSpace { get; set; } = 300;
+        public int Speed { get; set; } = 120;
 
         private float timeSinceSpawn = 6;
+        private int height = 800;
+        private int width = 100;
+        private FlappyKalle flappyKalle;
 
-        public PipeSpawner(Point position, Game game) :base(position, game)
+        private Random random = new Random();
+        public PipeSpawner(Point position, Game game, FlappyKalle flappyKalle) :base(position, game)
         { 
-            
+            this.flappyKalle = flappyKalle;
         }
 
         protected override void OnUpdate(float deltaTime)
@@ -31,6 +40,15 @@ namespace PopupMiniGames.GameAssets.FlappyKalleAssets
 
         private void SpawnPipe()
         {
+            timeSinceSpawn = 0;
+            int y = random.Next(MinSpawnY, MaxSpawnY);
+
+            Point bottomSpawn = new Point(SpawnX, y);
+            Point upperSpawn = new Point(SpawnX, y+MiddleSpace+height);
+            Pipe bottomPipe = new Pipe(bottomSpawn, Speed, Game, height, width, flappyKalle);
+            Game.AddGameObject(bottomPipe);
+            Pipe upperPipe = new Pipe(upperSpawn, Speed, Game, height, width, flappyKalle);
+            Game.AddGameObject(upperPipe);
 
         }
     }
