70a3a7fe43c102291397c3199e3c0bb6cc224402
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-19 13:58:14 +0100

    Created GameRenderer class, currently lacking logic

---
+++ PopupMiniGames/GameAssets/PolarEngine/GameRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace PopupMiniGames.GameAssets.PolarEngine
+{
+    public class GameRenderer : PictureBox
+    {
+        public List<Sprite> Sprites { get; private set; }
+        [System.ComponentModel.DesignerSerializationVisibility(System.ComponentModel.DesignerSerializationVisibility.Visible)] //ingen aning vad det betyder, varför det behövs, men fixade rött ._.
+        public Color BackgroundColor { get; set; }
+
+        public GameRenderer()
+        {
+            Sprites = new List<Sprite>();
+            BackgroundColor = Color.Black;
+        }
+
+        public void AddSprite(Sprite sprite) => Sprites.Add(sprite);
+        public void RemoveSprite(Sprite sprite) => Sprites.Remove(sprite);
+    }
+}
