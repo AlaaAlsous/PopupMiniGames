6c7f2adaa34e0800b5694084b0997c83cd8f05ce
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-19 14:03:19 +0100

    Created Component Class

---
+++ PopupMiniGames/GameAssets/PolarEngine/Component.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopupMiniGames.GameAssets.PolarEngine
+{
+    public abstract class Component
+    {
+        public GameObject Parent { get; set; }
+        public Component(GameObject parent) { Parent = parent; }
+    }
+}
