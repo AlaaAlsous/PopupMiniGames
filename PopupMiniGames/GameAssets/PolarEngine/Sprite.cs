71f9a355b514b54192e6ab0a8a39cc6f0bb80fc0
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-19 13:55:26 +0100

    Created Sprite class (image with rotation) and constructor for it

---
+++ PopupMiniGames/GameAssets/PolarEngine/Sprite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace PopupMiniGames.GameAssets.PolarEngine
+{
+    public class Sprite
+    {
+        public Image SpriteImage { get; set; }
+        public Point Position { get; set; }
+        public int Rotation { get; set; }
+
+        public Sprite(Point position, string? filePath = null, int rotation = 0)
+        {
+            Position = position;
+            Rotation = rotation;
+
+            if (!string.IsNullOrEmpty(filePath) && File.Exists(filePath))
+            {
+                SpriteImage = Image.FromFile(filePath);
+            }
+            else
+            {
+                Bitmap bmp = new Bitmap(500, 500);
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.Clear(Color.Transparent);
+                }
+                SpriteImage = bmp;
+            }
+        }
+    }
+}
