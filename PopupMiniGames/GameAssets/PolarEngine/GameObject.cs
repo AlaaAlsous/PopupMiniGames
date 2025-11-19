210eb669134bd4523b9c15327f04cbcc3e26dc3b
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-19 14:02:32 +0100

    Created GameObject class

---
+++ PopupMiniGames/GameAssets/PolarEngine/GameObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace PopupMiniGames.GameAssets.PolarEngine
+{
+    public class GameObject
+    {
+        public Point Position { get; set; }
+        public List<Component> Components { get; private set; }
+
+        public GameObject(Point position)
+        {
+            Position = position;
+            Components = new List<Component>();
+        }
+        public void AddComponent(Component component) => Components.Add(component);
+
+    }
+}
