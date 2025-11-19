8b542e6420e2988c1c7c30f8e6da46d63f7960d3
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-19 14:56:03 +0100

    Created PipeSpawner class

---
+++ PopupMiniGames/GameAssets/FlappyKalleAssets/PipeSpawner.cs
@@ -0,0 +1,37 @@
+﻿using MiniGames.MiniGames;
+using PopupMiniGames.GameAssets.PolarEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopupMiniGames.GameAssets.FlappyKalleAssets
+{
+    internal class PipeSpawner : GameObject
+    {
+        public float TimeBetweenSpawns { get; set; } = 6;
+
+        private float timeSinceSpawn = 6;
+
+        public PipeSpawner(Point position, Game game) :base(position, game)
+        { 
+            
+        }
+
+        protected override void OnUpdate(float deltaTime)
+        {
+            timeSinceSpawn += deltaTime;
+            if (timeSinceSpawn > TimeBetweenSpawns)
+            { 
+
+                SpawnPipe();
+            }
+        }
+
+        private void SpawnPipe()
+        {
+
+        }
+    }
+}
