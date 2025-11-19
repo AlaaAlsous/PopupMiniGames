423e67ebc5169dd4912f2ee762459ae22989bcdc
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-19 14:49:13 +0100

    Added FlappyController, and sprite for player

---
+++ PopupMiniGames/GameAssets/FlappyKalleAssets/FlappyController.cs
@@ -0,0 +1,46 @@
+﻿using MiniGames.MiniGames;
+using PopupMiniGames.GameAssets.PolarEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopupMiniGames.GameAssets.FlappyKalleAssets
+{
+    internal class FlappyController : GameObject
+    {
+        private float velocity = 0;
+        private float gravity = 30;
+        private Sprite sprite;
+        private Collider collider;
+
+        public FlappyController(Point position, Game game, string imageFilePath) : base(position, game)
+        {
+            Position = new Point(100, 300);
+            sprite = new Sprite(this, Point.Empty, imageFilePath);
+            AddComponent(sprite);
+
+            collider = new Collider(this);
+            //sprite is 120x120, this gives 20 pixel room on each side that doesn't have collision
+            collider.width = 80;
+            collider.height = 80;
+            collider.position = new Point(-40, -40);
+            AddComponent(collider);
+        }
+
+
+        override protected void OnUpdate(float deltaTime)
+        {
+            velocity += gravity * deltaTime;
+
+            int x = this.Position.X;
+            int y = this.Position.Y + (int)velocity;
+            y = Math.Clamp(y, 0, 900);
+            this.Position = new Point(x, y);
+
+            sprite.Rotation = Math.Clamp((int)velocity * 3, -50, 50);
+        }
+
+    }
+}
