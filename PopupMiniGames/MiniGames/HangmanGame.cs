e192bfcc24bba1274342abf5bceff0d29232209d
Alaa Alsous <lawyeralaa@gmail.com>
2025-11-19 10:24:46 +0100

    Add HangmanGame main form

--- PopupMiniGames/MiniGames/HangmanGame.cs
+++ PopupMiniGames/MiniGames/HangmanGame.cs
@@ -32,5 +32,58 @@ namespace MiniGames.MiniGames
             "APPLE", "HOUSE", "BRAIN", "LIGHT", "SOUND","HUMAN",
             "WATER", "STONE", "SMILE", "PLANT", "HEART",
         };
+        public HangmanGame()
+        {
+            this.Text = "Hangman Game";
+            this.Size = new Size(500, 300);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.Bisque;
+            this.ShowInTaskbar = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            labelWord = new Label()
+            {
+                Font = new Font("Consolas", 22, FontStyle.Bold),
+                Location = new Point(160, 75),
+                AutoSize = true
+            };
+            labelInfo = new Label()
+            {
+                Location = new Point(175, 160),
+                Font = new Font("Arial", 14),
+                AutoSize = true
+            };
+            inputBox = new TextBox()
+            {
+                Location = new Point(110, 220),
+                Size = new Size(110, 70),
+                Font = new Font("Arial", 15),
+            };
+            guessButton = new Button()
+            {
+                Text = "Guess",
+                Location = new Point(260, 220),
+                Size = new Size(130, 39)
+            };
+            currentDifficultyLabel = new Label()
+            {
+                Location = new Point(325, 20),
+                Font = new Font("Arial", 12),
+                AutoSize = true,
+                ForeColor = Color.OliveDrab,
+                Text = "Test"
+            };
+            inputBox.KeyDown += (s, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    guessButton.PerformClick();
+                }
+            };
+            this.Controls.Add(currentDifficultyLabel);
+            this.Controls.Add(labelWord);
+            this.Controls.Add(labelInfo);
+            this.Controls.Add(inputBox);
+            this.Controls.Add(guessButton);
+        }
     }
 }
