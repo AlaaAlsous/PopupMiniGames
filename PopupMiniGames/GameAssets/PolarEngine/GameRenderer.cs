a87ecd90712862b0f0cd54f5daae8863dc366bb9
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2025-11-19 13:59:38 +0100

    Added drawing logic to GameRenderer

--- PopupMiniGames/GameAssets/PolarEngine/GameRenderer.cs
+++ PopupMiniGames/GameAssets/PolarEngine/GameRenderer.cs
@@ -13,11 +13,35 @@ namespace PopupMiniGames.GameAssets.PolarEngine
         public List<Sprite> Sprites { get; private set; }
         [System.ComponentModel.DesignerSerializationVisibility(System.ComponentModel.DesignerSerializationVisibility.Visible)] //ingen aning vad det betyder, varför det behövs, men fixade rött ._.
         public Color BackgroundColor { get; set; }
+        private Bitmap canvas;
 
         public GameRenderer()
         {
             Sprites = new List<Sprite>();
             BackgroundColor = Color.Black;
+            canvas = new Bitmap(Width, Height);
+        }
+        public void UpdateFrame()
+        {
+            if (canvas.Width != Width || canvas.Height != Height)
+            {
+                canvas = new Bitmap(Width, Height);
+            }
+            using (Graphics g = Graphics.FromImage(canvas))
+            {
+                g.Clear(BackgroundColor);
+                for (int i = 0; i < Sprites.Count; i++)
+                {
+                    Sprite s = Sprites[i];
+                    Point pos = s.Position;
+
+                    g.TranslateTransform(pos.X, pos.Y);
+                    g.RotateTransform(s.Rotation);
+                    g.DrawImage(s.SpriteImage, -s.SpriteImage.Width / 2, -s.SpriteImage.Height / 2);
+                    g.ResetTransform();
+                }
+                Image = canvas;
+            }
         }
 
         public void AddSprite(Sprite sprite) => Sprites.Add(sprite);
