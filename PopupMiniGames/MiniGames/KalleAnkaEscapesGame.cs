f42cb360c41d202c1f4fbcd90160a3e177616a18
Alaa Alsous <lawyeralaa@gmail.com>
2025-11-20 18:17:10 +0100

    Create KalleAnkaEscapesGame class and game assets

---
+++ PopupMiniGames/MiniGames/KalleAnkaEscapesGame.cs
@@ -0,0 +1,24 @@
+﻿using MiniGames;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MiniGames.MiniGames
+{
+    public class KalleAnkaEscapesGame : Form, IMiniGameWithCleanup
+    {
+        public event EventHandler<GameResult>? GameEnded;
+        private PictureBox kalle;
+        private List<PictureBox> obstacles = new List<PictureBox>();
+        private Random random = new Random();
+        private System.Windows.Forms.Timer timer;
+        private Label livesLabel, scoreLabel, currentDifficultyLabel;
+        private int lives, speed, score, maxScore;
+        private Difficulty currentDifficulty;
+        private string assetsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\GameAssets");
+        private GameData gameData;
+        private GameResult result = new GameResult();
+    }
+}
